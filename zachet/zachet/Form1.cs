fc83402e78afb388a052b98401e6064a081b82c0
sabina-r <saby.r@yandex.ru>
2021-05-31 11:26:35 +0300

    added

---
+++ zachet/zachet/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zachet
+{
+    public partial class Билет13 : Form
+    {
+
+        public Билет13()
+        {
+            InitializeComponent();
+        }
+
+        Point click;
+        Graphics g;
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            g = CreateGraphics();
+
+            if (e.Button == MouseButtons.Left)
+            {
+                click = e.Location;
+                g.DrawRectangle(Pens.Black, click.X, click.Y, 200, 200);
+            }
+        }
+    }
+}
